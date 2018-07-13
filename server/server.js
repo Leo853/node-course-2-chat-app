749315dba021ce08412fec8b356d50eed79c1281
Leonid Trusov <leonid.trusov@kp.org>
2018-07-12 22:10:01 -0600

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'mike@example.com',
+    text: 'Hey What is going on.',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', (reason) => {
     console.log('User disconnected:', reason);
   });
