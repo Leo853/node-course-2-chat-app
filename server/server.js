eea9b27233998095f51ea099e515df3075d7ad4a
Leonid Trusov <leonid.trusov@kp.org>
2018-07-15 11:09:49 -0600

    added time to messages

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   //greet connected user
-  socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app!'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
 
   //inform others
-  socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
